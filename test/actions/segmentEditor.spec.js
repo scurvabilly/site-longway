55ea96ab3539b51cffdd8303bcf7b918fc39b63b
Andy Nygard <andreas.nygard@gmail.com>
2015-11-01 17:35:36 +1100

    Using Chai for assertions.

--- test/actions/segmentEditor.spec.js
+++ test/actions/segmentEditor.spec.js
@@ -1,4 +1,4 @@
-import expect from 'expect';
+import { expect } from 'chai';
 import * as types from '../../app/constants/ActionTypes'
 import * as actions from '../../app/actions/segmentEditor'
 
@@ -6,7 +6,7 @@ describe('segment editor actions', () => {
   it('addMarker should create MARKER_PLACED action', () => {
     expect(
       actions.addMarker({ lat: 123, lng: -321 })
-    ).toEqual({
+    ).to.deep.equal({
       type: types.MARKER_PLACED,
       position: { lat: 123, lng: -321 }
     })
@@ -15,7 +15,7 @@ describe('segment editor actions', () => {
   it('removeMarker should create MARKER_REMOVED action', () => {
     expect(
       actions.removeMarker(3)
-    ).toEqual({
+    ).to.deep.equal({
       type: types.MARKER_REMOVED,
       index: 3
     })
