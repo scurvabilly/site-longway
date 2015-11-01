b49c645812278b36a5a9009797b7b5602c8983c9
Andy Nygard <andreas.nygard@gmail.com>
2015-11-02 00:03:19 +1100

    Testing URL fetching in action creator, but no dice. Next stop: CORS!

--- app/actions/segmentEditor.js
+++ app/actions/segmentEditor.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch';
 import * as types from '../constants/ActionTypes';
 
 /**
@@ -6,7 +7,16 @@ import * as types from '../constants/ActionTypes';
  * @param {LatLng} position The marker coordinates.
  */
 export function addMarker(position) {
-  return { type: types.MARKER_PLACED, position }
+  return dispatch => {
+    // Temporary: Testing the thunk middleware by fetching a dummy URL prior to our action
+    fetch('http://www.google.com/').then(response => {
+      console.log('Success: ' + response);
+    }).catch(err => {
+      console.log('Error: ' + err);
+    }).then(() => {
+      dispatch({ type: types.MARKER_PLACED, position });
+    });
+  };
 }
 
 /**
