b2c4ddeceb0c23a6f10233d5374a99fd9985a339
Andy Nygard <andreas.nygard@gmail.com>
2015-11-06 02:25:48 +1100

    Action creators are now tested via spies.

--- test/actions/segmentEditor.spec.js
+++ test/actions/segmentEditor.spec.js
@@ -1,23 +1,44 @@
-import { expect } from 'chai';
+import expect from 'expect';
 import * as types from '../../app/constants/ActionTypes'
 import * as actions from '../../app/actions/segmentEditor'
 
+// Fixture for initial state fixture
+const initialState = {
+  defaultCenter: { lat: 100, lng: -1.5 },
+  defaultZoom: 1,
+  markers: [
+    { key: 1, position: { lat: 1, lng: 2 } },
+    { key: 2, position: { lat: 3, lng: 4 } },
+    { key: 3, position: { lat: 5, lng: 6 } }
+  ]
+};
+
 describe('segment editor actions', () => {
-  it('addMarker should create MARKER_PLACED action', () => {
-    expect(
-      actions.addMarker({ lat: 123, lng: -321 })
-    ).to.deep.equal({
+  it('addMarker should create MARKER_PLACED action, then request a route', () => {
+    const f = actions.addMarker({ lat: 123, lng: -321 })
+    expect(f).toBeA('function');
+
+    const dispatch = expect.createSpy();
+    f(dispatch, () => initialState);
+    expect(dispatch).toHaveBeenCalledWith({
       type: types.MARKER_PLACED,
       position: { lat: 123, lng: -321 }
-    })
+    }).toHaveBeenCalledWith({
+      type: types.ROUTE_REQUESTED
+    });
   })
 
   it('removeMarker should create MARKER_REMOVED action', () => {
-    expect(
-      actions.removeMarker(3)
-    ).to.deep.equal({
+    const f = actions.removeMarker(3);
+    expect(f).toBeA('function');
+
+    const dispatch = expect.createSpy();
+    f(dispatch, () => initialState);
+    expect(dispatch).toHaveBeenCalledWith({
       type: types.MARKER_REMOVED,
       index: 3
-    })
+    }).toHaveBeenCalledWith({
+      type: types.ROUTE_REQUESTED
+    });
   })
 });
--- test/reducers/segmentEditor.spec.js
+++ test/reducers/segmentEditor.spec.js
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import expect from 'expect';
 import segmentEditor from '../../app/reducers/segmentEditor'
 import * as types from '../../app/constants/ActionTypes'
 
@@ -6,16 +6,16 @@ describe('segment editor reducers', () => {
 
   it('should provide an initial state', () => {
     const initial = segmentEditor(undefined, {});
-    expect(initial.defaultCenter.lat).to.be.a('number');
-    expect(initial.defaultCenter.lng).to.be.a('number');
-    expect(initial.defaultZoom).to.be.a('number');
-    expect(initial.markers.length).to.equal(0);
+    expect(initial.defaultCenter.lat).toBeA('number');
+    expect(initial.defaultCenter.lng).toBeA('number');
+    expect(initial.defaultZoom).toBeA('number');
+    expect(initial.markers.length).toEqual(0);
   })
 
   it('should handle MARKER_PLACED', () => {
     const placed = segmentEditor(undefined, { type: types.MARKER_PLACED, position: { lat: 100, lng: -1.5 } });
-    expect(placed.markers.length).to.equal(1);
-    expect(placed.markers[0].position).to.deep.equal({ lat: 100, lng: -1.5 });
+    expect(placed.markers.length).toEqual(1);
+    expect(placed.markers[0].position).toEqual({ lat: 100, lng: -1.5 });
   })
 
   it('should handle MARKER_REMOVED', () => {
@@ -30,12 +30,12 @@ describe('segment editor reducers', () => {
     };
 
     const removedOne = segmentEditor(initial, { type: types.MARKER_REMOVED, index: 1 });
-    expect(removedOne.markers.length).to.equal(2);
-    expect(removedOne.markers[1]).to.deep.equal({ key: 3, position: { lat: 5, lng: 6 } });
+    expect(removedOne.markers.length).toEqual(2);
+    expect(removedOne.markers[1]).toEqual({ key: 3, position: { lat: 5, lng: 6 } });
     const removedTwo = segmentEditor(removedOne, { type: types.MARKER_REMOVED, index: 1 });
-    expect(removedTwo.markers.length).to.equal(1);
-    expect(removedTwo.markers[0]).to.deep.equal({ key: 1, position: { lat: 1, lng: 2 } });
+    expect(removedTwo.markers.length).toEqual(1);
+    expect(removedTwo.markers[0]).toEqual({ key: 1, position: { lat: 1, lng: 2 } });
     const removedAll = segmentEditor(removedTwo, { type: types.MARKER_REMOVED, index: 0 });
-    expect(removedAll.markers.length).to.equal(0);
+    expect(removedAll.markers.length).toEqual(0);
   })
 });
